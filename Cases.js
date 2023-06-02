9cb7ad380a3f027111e22967ff07c9147c298ec8
Manarr12 <mmmalqabbani@sm.imamu.edu.sa>
2023-06-02 22:26:14 +0300

    Activation learn more abou the cases button

---
+++ Cases.js
@@ -0,0 +1,8 @@
+const formBokingBtn = 
+bookingBtn = document.querySelector("#booking-button"),
+bookingFormCloseBtn = document.querySelector("#booking-form-close-button"),
+bookingContainer = document.querySelector("#booking-container");
+
+
+bookingBtn.addEventListener("click", () => bookingContainer.classList.add("show"));
+bookingFormCloseBtn.addEventListener("click", () => bookingContainer.classList.remove("show"));
