d11d2632347928546c19987399686b8400b0754b
mehafallazzam <mehafalla@gmail.com>
2023-06-03 01:59:41 +0300

    add case - book appointment

--- Cases.js
+++ Cases.js
@@ -6,3 +6,4 @@ bookingContainer = document.querySelector("#booking-container");
 
 bookingBtn.addEventListener("click", () => bookingContainer.classList.add("show"));
 bookingFormCloseBtn.addEventListener("click", () => bookingContainer.classList.remove("show"));
+
